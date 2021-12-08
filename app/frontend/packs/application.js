367cf51a25aa6eddc8f8469fc893abece514dd05
maks <mkozaev@mail.ru>
2021-12-08 20:54:44 +0300

    image fixes

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,9 +1,12 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../styles/aplication.scss'
+import '../styles/application.scss'
+// const images = require.context('../images', true)
+const images = require.context('../../frontend/images', true)
+const imagePath = (name) => images(name, true)
+
 import 'bootstrap/dist/js/bootstrap'
-const images = require.context('../images', true)
 
 Rails.start()
 ActiveStorage.start()
