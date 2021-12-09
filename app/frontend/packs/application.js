7819a0d8d7cbcc5fed15611be82b8f77d7226ba2
maks <mkozaev@mail.ru>
2021-12-09 21:33:05 +0300

    add webpacker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,7 +2,6 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../styles/application.scss'
-// const images = require.context('../images', true)
 const images = require.context('../../frontend/images', true)
 const imagePath = (name) => images(name, true)
 
@@ -10,7 +9,3 @@ import 'bootstrap/dist/js/bootstrap'
 
 Rails.start()
 ActiveStorage.start()
-
-
-
-
