d93183e47f1580652d38847dfe1b2a2182fe840f
maks <mkozaev@mail.ru>
2021-12-17 01:08:49 +0300

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user_can_edit?
+  helper_method :event_belong?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
@@ -17,5 +18,9 @@ class ApplicationController < ActionController::Base
       model.user == current_user ||
         (model.try(:event).present? && model.event.user == current_user)
     )
+  end
+
+  def event_belong?(event)
+    user_signed_in? || event.user == current_user
   end
 end
