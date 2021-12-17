ea7663bd6a053652a6c0403aa5fd4bef07796c84
maks <mkozaev@mail.ru>
2021-12-17 20:02:43 +0300

    add air-datepicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../styles/application.scss'
+import '../scripts/javascript.js'
 const images = require.context('../../frontend/images', true)
 const imagePath = (name) => images(name, true)
 
@@ -9,3 +10,7 @@ import 'bootstrap/dist/js/bootstrap'
 
 Rails.start()
 ActiveStorage.start()
+
+
+
+
