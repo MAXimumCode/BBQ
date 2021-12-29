f22d9f0bd7c4ed8e001151ea8aafca52a45d5165
maks <mkozaev@mail.ru>
2021-12-29 13:21:42 +0300

    add lightbox

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "#{t '.new_subscription'} #{event.title}"
+    mail to: event.user.email, subject: "#{default_i18n_subject} #{event.title}"
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "#{t '.new_comment'} #{event.title}"
+    mail to: email, subject: "#{default_i18n_subject} #{event.title}"
   end
 
   def photo(event, photo, email)
     @photo = photo
     @event = event
 
-    mail to: email, subject: "#{t '.new_photo'} #{event.title}"
+    mail to: email, subject: "#{default_i18n_subject} #{event.title}"
   end
 end
