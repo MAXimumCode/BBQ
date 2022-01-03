c17eb86637a49abe7a1f7a8b243f2689df6af406
maks <mkozaev@mail.ru>
2022-01-03 15:38:43 +0300

    add password recovery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: '', # не используйте для тестов свои реальные ящики
-    password: '', # не храните здесь пароль!
+    user_name: ENV['DEV_GOOGLE_USERNAME'], # не используйте для тестов свои реальные ящики
+    password: ENV['DEV_GOOGLE_PASSWORD'], # не храните здесь пароль!
     authentication: 'plain',
     enable_starttls_auto: true
   }
