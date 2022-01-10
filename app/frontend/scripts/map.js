4114086303e8ddaacfa10e54c41eb9acd00ea6ca
maks <mkozaev@mail.ru>
2022-01-10 15:40:21 +0300

    js refactor

--- app/frontend/scripts/map.js
+++ app/frontend/scripts/map.js
@@ -1,33 +1,35 @@
-window.onload = function() {
-  ymaps.ready (init);
+ymaps.ready(init);
 
-  function init () {
-    const address = document.getElementById ('map').getAttribute ('data-address');
+function init() {
+  if (!document.getElementById('map')) return;
+  const address = document.getElementById('map').getAttribute('data-address');
 
-    const myMap = new ymaps.Map ("map", {
-      center: [55.76, 37.64],
-      zoom: 10
-    });
+  const myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 10
+  });
 
-    const myGeocoder = ymaps.geocode (address);
+  const myGeocoder = ymaps.geocode(address);
 
-    myGeocoder.then (
-      function (res) {
-        const coordinates = res.geoObjects.get (0).geometry.getCoordinates ();
+  myGeocoder.then(
+    function(res) {
+      const coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-        myMap.geoObjects.add (
-          new ymaps.Placemark (
-            coordinates,
-            { iconContent: address },
-            { preset: 'islands#blueStretchyIcon' }
-          )
-        );
+      myMap.geoObjects.add(
+        new ymaps.Placemark(
+          coordinates, {
+            iconContent: address
+          }, {
+            preset: 'islands#blueStretchyIcon'
+          }
+        )
+      );
 
-        myMap.setCenter (coordinates);
-        myMap.setZoom (15);
-      }, function (err) {
-        alert ('Ошибка при определении местоположения');
-      }
-    );
-  }
+      myMap.setCenter(coordinates);
+      myMap.setZoom(15);
+    },
+    function(err) {
+      alert('Ошибка при определении местоположения');
+    }
+  );
 }
