aec7080ce56e9f5e85a453ffe80b0f49afb3df30
maks <mkozaev@mail.ru>
2022-01-12 20:20:43 +0300

    error fix

--- app/frontend/scripts/glide.js
+++ app/frontend/scripts/glide.js
@@ -1,22 +1,24 @@
 import Glide from '@glidejs/glide'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const glide = new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    focusAt: 'center',
-    perView: 6,
-    animationDuration: 800,
-    animationTimingFunc: 'ease-in-out',
-    breakpoints: {
-      1024: {
-        perView: 3
-      },
-      600: {
-        perView: 2
+  if(document.querySelector('.glide')) {
+    const glide = new Glide('.glide', {
+      type: 'carousel',
+      startAt: 0,
+      focusAt: 'center',
+      perView: 6,
+      animationDuration: 800,
+      animationTimingFunc: 'ease-in-out',
+      breakpoints: {
+        1024: {
+          perView: 3
+        },
+        600: {
+          perView: 2
+        }
       }
-    }
-  })
+    })
 
-  glide.mount()
+    glide.mount()
+  }
 })
