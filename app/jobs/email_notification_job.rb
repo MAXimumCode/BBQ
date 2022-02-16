2737e6bdad804352b0aa6ed444e51ef07a9d0923
maks <mkozaev@mail.ru>
2022-02-16 22:22:57 +0300

    refactor mailjob

---
+++ app/jobs/email_notification_job.rb
@@ -0,0 +1,16 @@
+class EmailNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(object)
+    event = object.event
+
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [object.user&.email]).uniq
+
+    case object
+    when Comment
+      all_emails.each { |mail| EventMailer.comment(event, object, mail).deliver_later }
+    else
+      all_emails.each { |mail| EventMailer.photo(event, object, mail).deliver_later }
+    end
+  end
+end
